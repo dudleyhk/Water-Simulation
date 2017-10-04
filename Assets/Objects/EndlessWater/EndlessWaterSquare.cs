2995384d9e2f66b3c5a6d223a4a3bd5e5c630938
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-10-04 22:58:48 +0100

    generating mesh. The huge mesh is generating but the shader wont add to it properly... might try and add my shader to it.

---
+++ Assets/Objects/EndlessWater/EndlessWaterSquare.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EndlessWaterSquare : MonoBehaviour
+{
+    public GameObject boatObj;
+    public GameObject waterSquareObj;
+
+    // Water square data
+    private float squareWidth = 800f;
+    private float innerSquareResolution = 5f;
+    private float outerSquareResolution = 25f;
+
+    List<WaterSquare> waterSquares = new List<WaterSquare>();
+
+    // For Thread
+    private float secondsSinceStart;
+    private Vector3 boatPos;
+
+    // The position of the ocean has to be updated in the thread because it follows
+    //  the boat. Is not the same as pos of boat because it moves with the same resolution as the smallest water square resolution
+    private Vector3 oceanPos;
+
+    private bool hasThreadUpdatedWater;
+
+    private void Start()
+    {
+        CreateEndlessSea();
+
+        secondsSinceStart = Time.time;
+
+        ThreadPool.QueueUserWorkItem(new WaitCallback(UpdateWaterWithThreadPooling));
+
+        StartCoroutine(UpdateWater());
+    }
+
+
+    private void Update()
+    {
+        // UpdateNoThread();
+
+        secondsSinceStart = Time.time;
+
+        boatPos = boatObj.transform.position;
+    }
+
+
+    private void UpdateWaterNoThread()
+    {
+        boatPos = boatObj.transform.position;
+
+        MoveWaterToBoat();
+
+        transform.position = oceanPos;
+
+        for(int i = 0; i < waterSquares.Count; i++)
+        {
+            waterSquares[i].MoveSea(oceanPos, Time.time);
+        }
+    }
+
+    //The loop that gives the updated vertices from the thread to the meshes
+    //which we can't do in its own thread
+    private IEnumerator UpdateWater()
+    {
+        while(true)
+        {
+            // has the thread finsihed updating the water
+            if(hasThreadUpdatedWater)
+            {
+                // Move the water to the boat
+                transform.position = oceanPos;
+
+                // Add the updated vertices to the water meshes
+                for(int i = 0; i < waterSquares.Count; i++)
+                {
+                    waterSquares[i].terrainMeshFilter.mesh.vertices = waterSquares[i].vertices;
+                    waterSquares[i].terrainMeshFilter.mesh.RecalculateNormals();
+                }
+
+                // stop looping until we have updated the water in the thread
+                hasThreadUpdatedWater = false;
+
+                ThreadPool.QueueUserWorkItem(new WaitCallback(UpdateWaterWithThreadPooling));
+
+            }
+
+            /// Dont need to update the water every frame. 
+            yield return new WaitForSeconds(Time.deltaTime * 3f);
+        }
+    }
+
+
+    private void UpdateWaterWithThreadPooling(object state)
+    {
+        MoveWaterToBoat();
+
+        // Loop through all the water squares. 
+        for(int i = 0; i < waterSquares.Count; i++)
+        {
+            // the local centre pos of this square
+            Vector3 centrePos = waterSquares[i].centrePos;
+            Vector3[] vertices = waterSquares[i].vertices;
+            
+            // Update the vertices in this square
+            for(int j = 0; j < vertices.Length; j++)
+            {
+                Vector3 vertexPos = vertices[j];
+
+                //Can't use transformpoint in a thread, so to find the global position of the vertex
+                //we just add the position of the ocean and the square because rotation and scale is always 0 and 1
+                Vector3 vertexPosGlobal = vertexPos + centrePos + oceanPos;
+
+                // Get the water height
+                vertexPos.y = WaterController.instance.GetWaveYPos(vertexPosGlobal, secondsSinceStart);
+
+                // Save the new y coordinate but x and z are still in local position
+                vertices[j] = vertexPos;
+            }
+        }
+        hasThreadUpdatedWater = true;
+    }
+
+
+    private void MoveWaterToBoat()
+    {
+        // Round to nearest resolution
+        float x = innerSquareResolution * (int)Mathf.Round(boatPos.x / innerSquareResolution);
+        float z = innerSquareResolution * (int)Mathf.Round(boatPos.z / innerSquareResolution);
+
+        // Should we move the water?
+        if(oceanPos.x != x || oceanPos.z != z)
+        {
+            oceanPos = new Vector3(x, oceanPos.y, z);
+        }
+    }
+
+
+    private void CreateEndlessSea()
+    {
+        // the centre piece
+        AddWaterPlane(0f, 0f, 0f, squareWidth, innerSquareResolution);
+
+        // The 8 squares around the centre square. 
+        for(int x = -1; x <= 1; x += 1)
+        {
+            for(int z = -1; z <= 1; z += 1)
+            {
+                // ignore the centre square
+                if (x == 0 && z == 0)
+                {
+                    continue;
+                }
+
+                // the y-pos should be lower than the square with high res to avoid an ugly seam
+                float yPos = -0.5f;
+                AddWaterPlane(x * squareWidth, z * squareWidth, yPos, squareWidth, outerSquareResolution);
+            }
+        }
+    }
+
+
+
+    private void AddWaterPlane(float xCoord, float zCoord, float yPos, float squareWidth, float spacing)
+    {
+        var waterPlane = Instantiate(waterSquareObj, transform.position, transform.rotation) as GameObject;
+
+        waterPlane.SetActive(true);
+
+        // change its position
+        Vector3 centrePos = transform.position;
+        centrePos.x += xCoord;
+        centrePos.y = yPos;
+        centrePos.z += zCoord;
+
+        waterPlane.transform.position = centrePos;
+
+        // parent it.
+        waterPlane.transform.parent = transform;
+
+        // give it moving water properties and set its width and resolution to generate the water mesh.
+        WaterSquare tempWaterSquare = new WaterSquare(waterPlane, squareWidth, spacing);
+        waterSquares.Add(tempWaterSquare);
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ Assets/Objects/EndlessWater/WaterSquare.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSquare : MonoBehaviour
+{
+    public Transform squareTransform;
+
+    // Add the wave mesh to the MeshFilter.
+    public MeshFilter terrainMeshFilter;
+
+    // the total size in m
+    private float size;
+
+    // Res = width of one square
+    private float spacing;
+
+    // total number of vertices we need to generate based on size and spacing.
+    private int width;
+
+    // For the thread to update the water.
+    // The local centre position of this square to fake transform point in a thread.
+    public Vector3 centrePos;
+
+    // The latest vertices that belong to this square. 
+    public Vector3[] vertices;
+
+    public WaterSquare(GameObject waterSquareObj, float size, float spacing)
+    {
+        this.squareTransform = waterSquareObj.transform;
+        this.size = size;
+        this.spacing = spacing;
+        this.terrainMeshFilter = squareTransform.GetComponent<MeshFilter>();
+
+        // Calc the data we need to generate the water mesh
+        width = (int)(size / spacing);
+
+        // because each square is 2 vertices, so we need one more. 
+        width += 1;
+
+        // Centre the sea
+        float offset = -((width - 1) * spacing) / 2;
+
+        Vector3 newPos = new Vector3(offset, squareTransform.position.y, offset);
+
+        squareTransform.position += newPos;
+
+        //Save the centre position of the square.
+        this.centrePos = waterSquareObj.transform.localPosition;
+
+        // Genertate the sea.
+        // To calculate the position of the square.
+        float startTime = System.Environment.TickCount;
+
+        GenerateMesh();
+
+        // Calculate the time it took to generate the terrain in seconds
+        float timeToGenerateSea = (System.Environment.TickCount - startTime) / 1000f;
+
+        print("The sea was generated in " + timeToGenerateSea.ToString());
+
+        this.vertices = terrainMeshFilter.mesh.vertices;
+    }
+
+
+    // If we are update the square from outside of a thread.
+    public void MoveSea(Vector3 oceanPos, float timeSinceStart)
+    {
+        Vector3[] vertices = terrainMeshFilter.mesh.vertices;
+
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = vertices[i];
+
+            // From local to global 
+            // Vector3 vertexGlobal = squareTransform.TransformPoint(vertex);
+
+            Vector3 vertexGlobal = vertex + centrePos + oceanPos;
+
+            // DEBUG
+            if(i == 0)
+            {
+                // Debug.Log(vertexGlobal + " " + vertexGlobalTest);
+            }
+
+            // Get the water height at this coordinate
+            vertex.y = WaterController.instance.GetWaveYPos(vertexGlobal, timeSinceStart);
+
+            vertices[i] = vertex;
+        }
+
+        terrainMeshFilter.mesh.vertices = vertices;
+        terrainMeshFilter.mesh.RecalculateNormals();
+    }
+
+
+    public void GenerateMesh()
+    {
+        List<Vector3[]> verts = new List<Vector3[]>();
+
+        // Triangle list. 
+        List<int> tris = new List<int>();
+
+        // Uvs
+        // List<Vector2> uvs = new List<Vector2>();
+
+        for (int z = 0; z < width; z++)
+        {
+            verts.Add(new Vector3[width]);
+            for (int x = 0; x < width; x++)
+            {
+                Vector3 currentPoint = new Vector3();
+
+                // Get the coordinates of the vertices;
+                currentPoint.x = x * spacing;
+                currentPoint.z = z * spacing;
+                currentPoint.y = squareTransform.position.y;
+
+                verts[z][x] = currentPoint;
+
+                //UVs.Add(new Vector2(x, z));
+
+                // Dont generate a triangle the first coordinate on each row.
+                // Because thats just one point. 
+
+                if (x <= 0 || z <= 0)
+                {
+                    continue;
+                }
+                // Create the quad.
+                // each square consists of 2 triangles
+                // The triangle south-west of the vertice
+                tris.Add(x + z * width);                        //          
+                tris.Add(x + (z - 1) * width);                  //          
+                tris.Add((x - 1) + (z - 1) * width);            //          
+                                                                //          
+                // the trianle west-south of the vertice        //          
+                tris.Add(x + z * width);                        //          
+                tris.Add((x - 1) + (z - 1) * width);            //          
+                tris.Add((x - 1) + z * width);                  //          
+            }
+        }
+
+        // Unfold the 2D array of vertices into a 1D array
+        Vector3[] unfoldedVerts = new Vector3[width * width];
+
+        int idx = 0;
+        foreach (var v in verts)
+        {
+            //copies all the elements of the current 1D-array to the specifies 1D array.
+            v.CopyTo(unfoldedVerts, idx * width);
+            idx++;
+        }
+
+        // Generate the mesh object.
+        Mesh newMesh = new Mesh();
+        newMesh.vertices = unfoldedVerts;
+        // newMesh.uv = uvs.ToArray();
+        newMesh.triangles = tris.ToArray();
+
+        // Ensure the bounding volume is correct.
+        newMesh.RecalculateBounds();
+
+        // Update the normals to reflect the change. 
+        newMesh.RecalculateNormals();
+
+        // Add the generated mesh to this GameObject.
+        terrainMeshFilter.mesh.Clear();
+        terrainMeshFilter.mesh = newMesh;
+        terrainMeshFilter.mesh.name = "Water Mesh";
+
+        print(terrainMeshFilter.mesh.vertices.Length);
+    }
+}
